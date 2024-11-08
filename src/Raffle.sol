a12006998a7aa5d240bbeffdc6ced6cc4046fc5f
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-11-08 18:32:25 +0530

    [Johny] Added events for entered into raffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -6,6 +6,8 @@ contract Raffle {
     address payable[] private s_players;
     uint256 private immutable i_entranceFee;
 
+    event EnteredRaffle(address indexed player);
+
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
@@ -13,6 +15,7 @@ contract Raffle {
     function enterRaffle() public payable {
         if ( msg.value < i_entranceFee ) revert Raffle_NotEnoughEthSent();
         s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
     }
 
     function pickWinner() public {}
