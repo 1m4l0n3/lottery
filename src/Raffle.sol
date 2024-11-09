a30aae82ebe29bae04abaf9be84d6f0c39f90d83
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-11-09 11:07:30 +0530

    [Johny] Add interval to pick the winner

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -5,11 +5,15 @@ error Raffle_NotEnoughEthSent();
 contract Raffle {
     address payable[] private s_players;
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
 
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() public payable {
@@ -18,7 +22,9 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external view {
+        if (block.timestamp - s_lastTimeStamp < i_interval) revert();
+    }
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
