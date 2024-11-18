645816e4ac012cbb0000cb942f7369a67e1817ff
JohnyVasamsetti <vasamsetti.johny@gmail.com>
2024-11-18 13:33:13 +0530

    [Johny] Import VRF related contracts & Added variables to store request status

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,16 +1,29 @@
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
 error Raffle_NotEnoughEthSent();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
+    struct RequestStatus {
+        bool fulfilled;
+        bool exists;
+        uint256[] randomWords;
+    }
+
     address payable[] private s_players;
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
     uint256 private s_lastTimeStamp;
 
+    mapping( uint256 => RequestStatus ) public s_requests;
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
     event EnteredRaffle(address indexed player);
 
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
@@ -26,11 +39,11 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() external view {
-        if (block.timestamp - s_lastTimeStamp < i_interval) revert();
-    }
-
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override{
+
     }
 }
